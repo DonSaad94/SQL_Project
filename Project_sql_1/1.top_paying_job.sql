6e3a62405834763707de021d4e723158e42f3a4e
DonSaad94 <saadpt1994@gmail.com>
2025-12-12 03:20:40 +0300

    upload sql files

---
+++ Project_sql_1/1.top_paying_job.sql
@@ -0,0 +1,19 @@
+-- delete this later 
+  SELECT
+	job_id,
+	job_title,
+	job_location,
+	job_schedule_type,
+	salary_year_avg,
+	job_posted_date,
+NAME AS  company_name
+FROM
+	job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+	job_title LIKE '%Data Scientist%'
+	AND salary_year_avg IS NOT NULL
+	AND job_location = 'Anywhere'
+ORDER BY
+	salary_year_avg DESC 
+LIMIT 10
